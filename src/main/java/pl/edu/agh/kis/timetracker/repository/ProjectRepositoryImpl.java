ed17110b5b62866e3ff05ff169a2013920e5b2bf
Jan Pałucki <jan.palucki@ovoo.pl>
2018-04-26 15:27:05 +0200

    fixed getting projects file path

--- src/main/java/pl/edu/agh/kis/timetracker/repository/ProjectRepositoryImpl.java
+++ src/main/java/pl/edu/agh/kis/timetracker/repository/ProjectRepositoryImpl.java
@@ -4,8 +4,8 @@ import com.google.gson.Gson;
 import pl.edu.agh.kis.timetracker.domain.Project;
 import pl.edu.agh.kis.timetracker.domain.ProjectsConfig;
 
-import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.URL;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -13,22 +13,25 @@ import java.nio.file.Paths;
 
 public class ProjectRepositoryImpl implements ProjectRepository {
 
-    private final String PROJECTS_FILE = "src/main/resources/projects.json";
+    private final String PROJECTS_FILE = "projects.json";
 
     @Override
     public void saveProject(Project project) {
         try {
-            Path path = Paths.get(PROJECTS_FILE);
+            URL url = getClass().getClassLoader().getResource(PROJECTS_FILE);
+            Path path = Paths.get(url.toURI());
             byte[] encoded = Files.readAllBytes(path);
+
+
             String json = new String(encoded, Charset.forName("UTF-8"));
             ProjectsConfig config = new Gson().fromJson(json, ProjectsConfig.class);
 
             config.getProjects().add(project);
             String jsonToSave = new Gson().toJson(config).toString();
-            PrintWriter out = new PrintWriter(PROJECTS_FILE);
+            PrintWriter out = new PrintWriter(path.toString());
             out.println(jsonToSave);
             out.close();
-        } catch(IOException e){
+        } catch(Exception e){
             e.printStackTrace();
         }
     }
@@ -36,17 +39,18 @@ public class ProjectRepositoryImpl implements ProjectRepository {
     @Override
     public Project getProject(String projectName) {
         try {
-            byte[] encoded = Files.readAllBytes(Paths.get(PROJECTS_FILE));
+            URL url = getClass().getClassLoader().getResource(PROJECTS_FILE);
+            Path path = Paths.get(url.toURI());
+            byte[] encoded = Files.readAllBytes(path);
             String json = new String(encoded, Charset.forName("UTF-8"));
             ProjectsConfig config = new Gson().fromJson(json, ProjectsConfig.class);
-            System.out.println(config);
 
             for(Project project: config.getProjects()){
                 if(project.getName().equals(projectName)){
                     return project;
                 }
             }
-        } catch(IOException e){
+        } catch(Exception e){
             e.printStackTrace();
         }
 
