3991b004f46767007591a4261b2556ccc94004d1
Wojciech Kumoń <Wojciech.Kumon@sabre.com>
2018-04-26 14:16:21 +0200

    Time service task finishing

--- src/main/java/pl/edu/agh/kis/timetracker/service/TimeService.java
+++ src/main/java/pl/edu/agh/kis/timetracker/service/TimeService.java
@@ -13,6 +13,28 @@ public class TimeService {
   }
 
   public void start(Task task) {
+    if (isValidNotStartedTask(task)) {
+      throw new IllegalArgumentException();
+    }
     task.setStart(LocalDateTime.now(clock));
+  }
+
+  private boolean isValidNotStartedTask(Task task) {
+    return task.getStart() != null
+        || task.getFinish() != null;
+  }
+
+  public void finish(Task task) {
+    LocalDateTime finishTime = LocalDateTime.now(clock);
+    if (isValidStartedTask(task, finishTime)) {
+      throw new IllegalArgumentException();
+    }
+    task.setFinish(finishTime);
+  }
+
+  private boolean isValidStartedTask(Task task, LocalDateTime finishTime) {
+    return task.getStart() == null
+        || task.getStart().isAfter(finishTime)
+        || task.getFinish() != null;
   }
 }
