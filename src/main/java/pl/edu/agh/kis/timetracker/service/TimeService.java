ed3906e67c7f3c9f12ff55d912828c2f7670db9e
Wojciech Kumoń <Wojciech.Kumon@sabre.com>
2018-04-26 14:29:31 +0200

    Time service task finishing post review changes

--- src/main/java/pl/edu/agh/kis/timetracker/service/TimeService.java
+++ src/main/java/pl/edu/agh/kis/timetracker/service/TimeService.java
@@ -13,26 +13,26 @@ public class TimeService {
   }
 
   public void start(Task task) {
-    if (isValidNotStartedTask(task)) {
+    if (isInvalidValidNotStartedTask(task)) {
       throw new IllegalArgumentException();
     }
     task.setStart(LocalDateTime.now(clock));
   }
 
-  private boolean isValidNotStartedTask(Task task) {
+  private boolean isInvalidValidNotStartedTask(Task task) {
     return task.getStart() != null
         || task.getFinish() != null;
   }
 
   public void finish(Task task) {
     LocalDateTime finishTime = LocalDateTime.now(clock);
-    if (isValidStartedTask(task, finishTime)) {
+    if (isInvalidStartedTask(task, finishTime)) {
       throw new IllegalArgumentException();
     }
     task.setFinish(finishTime);
   }
 
-  private boolean isValidStartedTask(Task task, LocalDateTime finishTime) {
+  private boolean isInvalidStartedTask(Task task, LocalDateTime finishTime) {
     return task.getStart() == null
         || task.getStart().isAfter(finishTime)
         || task.getFinish() != null;
